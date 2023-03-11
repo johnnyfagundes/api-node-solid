d6e2887ca11cdcdb4590291592c6446d32176b8f
johnnysantos <johnny.santos@bairesdev.com.br>
2023-03-11 10:19:27 -0300

    Initial commit

---
+++ src/use-cases/erros/user-already-exists-error.ts
@@ -0,0 +1,6 @@
+export class UserAlreadyExistsError extends Error {
+  constructor() {
+    super('E-mail already exists')
+  }
+
+}
---
+++ src/use-cases/register.ts
@@ -0,0 +1,34 @@
+import { hash } from 'bcryptjs'
+import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+// SOLID
+
+// D - Dependency Inversion Principle
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash
+    })
+  }
+}
